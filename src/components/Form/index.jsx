820d035140c34805b2ecb5071eee1c9967c416ac
rafael-arashiro <145889621+rafael-arashiro@users.noreply.github.com>
2025-04-06 08:41:47 -0300

    Update index.jsx

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -15,7 +15,7 @@ function Form() {
     event.preventDefault();
 
     axios
-      .post("http://localhost:3000/api/v1/instituicoes", formData, {
+      .post("pi3-bti-22-back.vercel.app/api/v1/instituicoes", formData, {
         headers: {
           "Content-Type": "application/json",
         },
